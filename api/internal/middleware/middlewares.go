71f77f8886a54db82ebeaf84f873958a98d663c3
unknown <germanchrystan@gmail.com>
2022-03-31 22:10:37 -0300

    Middlewares

--- api/internal/middleware/middlewares.go
+++ api/internal/middleware/middlewares.go
@@ -1,13 +1,16 @@
 package middleware
 
 import (
-	"context"
 	"database/sql"
+
+	"github.com/Germanchrystan/GeekStore/api/internal/domain"
+	"github.com/Germanchrystan/GeekStore/api/pkg/web"
+	"github.com/gin-gonic/gin"
 )
 
 type Middleware interface {
-	isAdminUserSession(ctx context.Context, session string) error
-	isUserSession(ctx context.Context, session string) error
+	isAdminUserSession() gin.HandlerFunc
+	isUserSession() gin.HandlerFunc
 }
 
 //===================================================================================================//
@@ -23,13 +26,64 @@ func NewMiddlewareRepository(db *sql.DB) Middleware {
 }
 
 //===================================================================================================//
-func (m *middlewareRepository) isAdminUserSession(ctx context.Context, session string) error {
-	return nil
+func (m *middlewareRepository) isAdminUserSession() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		session := ctx.Request.Header.Get("session_id")
+
+		query := "SELECT * FROM sessions WHERE _id=$1"
+		row := m.db.QueryRow(query, session)
+
+		var userID string
+		err := row.Scan(&userID)
+		if err != nil {
+			ctx.JSON(400, web.NewResponse(400, nil, "Something went wrong"))
+			return
+		}
+
+		userQuery := "SELECT * FROM users WHERE _id=$1"
+		user := domain.User{}
+		row = m.db.QueryRow(userQuery, userID)
+		err = row.Scan(&user.ID, &user.Username, &user.FirstName, &user.LastName, &user.Email, &user.IsActive, &user.HashedPassword, &user.IsAdmin, &user.IsBanned)
+		if err != nil {
+			ctx.JSON(400, web.NewResponse(400, nil, "Something went wrong"))
+			return
+		}
+
+		if !user.IsAdmin {
+			ctx.JSON(400, web.NewResponse(400, nil, "This user cannot perform this action"))
+			return
+		}
+		ctx.Writer.Header().Set("user_id", user.ID)
+		ctx.Next()
+	}
 }
 
 //===================================================================================================//
-func (m *middlewareRepository) isUserSession(ctx context.Context, session string) error {
-	return nil
+func (m *middlewareRepository) isUserSession() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		session := ctx.Request.Header.Get("session_id")
+		query := "SELECT * FROM sessions WHERE _id=$1"
+		row := m.db.QueryRow(query, session)
+
+		var userID string
+		err := row.Scan(&userID)
+		if err != nil {
+
+		}
+
+		userQuery := "SELECT * FROM users WHERE _id=$1"
+		user := domain.User{}
+		row = m.db.QueryRow(userQuery, userID)
+		err = row.Scan(&user.ID, &user.Username, &user.FirstName, &user.LastName, &user.Email, &user.IsActive, &user.HashedPassword, &user.IsAdmin, &user.IsBanned)
+		if err != nil {
+
+		}
+
+		if user.IsActive {
+			ctx.Writer.Header().Set("user_id", user.ID)
+			ctx.Next()
+		}
+	}
 }
 
 //===================================================================================================//
