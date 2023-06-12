53e213f2f1bef2ee8f81916ba48634cee4230a1c
Germán Chrystan <56849875+Germanchrystan@users.noreply.github.com>
2023-06-12 22:41:48 +0000

    fetching

--- client/src/pages/profile/helpers/get-panel-data.ts
+++ client/src/pages/profile/helpers/get-panel-data.ts
@@ -8,23 +8,33 @@ import {
 import { CardProps } from '../../../components/card';
 
 const getUserPurchases = (userId: String): [] => {
-
+    fetch('./../../../mocks/user_purchases.json')
+    .then(res => res.json())
+    .then(data => console.log(data))
 };
 
 const getUserCustoms = (userId: String) => {
-
+    fetch('./../../../mocks/user_customs.json')
+    .then(res => res.json())
+    .then(data => console.log(data));
 };
 
 const getUserWhishlist = (userId: String) => {
-
+    fetch('./../../../mocks/user_whishlist.json')
+    .then(res => res.json())
+    .then(data => console.log(data));
 };
 
 const getUserAddresses = (userId: String) => {
-
+    fetch('./../../../mocks/user_addresses.json')
+    .then(res => res.json())
+    .then(data => console.log(data));
 };
 
 const getUserCards = (userId: String) => {
-
+    fetch('./../../../mocks/user_credit_cards.json')
+    .then(res => res.json())
+    .then(data => console.log(data));
 };
 
 const getPanelData = (selectedPanel : String, userId: String) => {
